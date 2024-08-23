9856ff9506c4da6035b6c7b8af6bba23d559af19
luiz-rissardi <rissardi.luiz2006@gmail.com>
2024-08-22 22:22:00 -0300

    Finaly the opentelemetry study

---
+++ tracing.js
@@ -0,0 +1,28 @@
+import {
+    diag,
+    DiagConsoleLogger,
+    DiagLogLevel
+} from '@opentelemetry/api'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http'
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ERROR)
+
+const sdk = new NodeSDK({
+    serviceName: 'students-api',
+    traceExporter: new OTLPTraceExporter({
+        url: 'http://localhost:4317',
+        compression: 'gzip'
+    }),
+    instrumentations: [
+        new HttpInstrumentation()
+    ]
+})
+
+process.on('beforeExit', async () => {
+    await sdk.shutdown()
+})
+
+export const initalizeTracing = async () => {
+    return sdk.start()
+}
